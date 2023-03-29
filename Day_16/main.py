4bfd2841dce0c11e65f0f381a0f0f113f9f0bc57
Kashif Ali Khan <113260067+kashifalikhan36@users.noreply.github.com>
2023-03-29 19:11:17 +0000

    pyhton_rev

---
+++ Day_16/main.py
@@ -0,0 +1,6 @@
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+ask_coffee=input("what kind of coffee u want ? :-").lower()
+ask_coffee=MenuItem.name()
+cost
