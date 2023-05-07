72c03543f291e6864fa117ef57edcc8243fee458
Kashif Ali Khan <113260067+kashifalikhan36@users.noreply.github.com>
2023-05-08 02:32:28 +0530

    Spotify And Amazon deal project

---
+++ Day_46/Top 100 Songs spotify Playlist Maker/main.py
@@ -0,0 +1,50 @@
+import requests
+from bs4 import BeautifulSoup
+import re
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+
+SPOTIPY_CLIENT_ID="ca1cf6ce5c8e40b5affdb83c2f9d2f1d"
+SPOTIPY_CLIENT_SECRET="6570debcf4a34eefa8f197936f3c5a03"
+sp = spotipy.Spotify(
+    auth_manager=SpotifyOAuth(
+        scope="playlist-modify-public playlist-modify-private",
+        redirect_uri="http://example.com",
+        client_id=SPOTIPY_CLIENT_ID,
+        client_secret=SPOTIPY_CLIENT_SECRET,
+        show_dialog=True,
+        cache_path="Day_46\\token.txt"
+    )
+)
+user_id = sp.current_user()["id"]
+
+date = input("Which year do you want to travel to? Type the date in this format YYYY-MM-DD: ")
+year = date.split("-")[0]
+month = date.split("-")[1]
+day = date.split("-")[2]
+
+response=requests.get(url=f"https://www.billboard.com/charts/hot-100/{year}-{month}-{day}/").text
+soup=BeautifulSoup(response,"lxml")
+all=soup.find_all("h3", id="title-of-a-story", class_="c-title a-no-trucate a-font-primary-bold-s u-letter-spacing-0021 lrv-u-font-size-18@tablet lrv-u-font-size-16 u-line-height-125 u-line-height-normal@mobile-max a-truncate-ellipsis u-max-width-330 u-max-width-230@tablet-only")
+top=soup.find("h3", id="title-of-a-story", class_="c-title a-no-trucate a-font-primary-bold-s u-letter-spacing-0021 u-font-size-23@tablet lrv-u-font-size-16 u-line-height-125 u-line-height-normal@mobile-max a-truncate-ellipsis u-max-width-245 u-max-width-230@tablet-only u-letter-spacing-0028@tablet")
+songs=[]
+for i in all:
+    songs.append(i.string)
+songs = [str(s) for s in songs]
+pattern = re.compile(r'^\W+|\W+$')
+clean_tags = [re.sub(pattern, '', tag) for tag in songs]
+clean_tags.insert(0,re.sub(pattern, '', top.string))
+
+songs=[]
+for song in clean_tags:
+    result = sp.search(q=f"track:{song} year:{year}", type="track")
+    try:
+        uri = result["tracks"]["items"][0]["uri"]
+        songs.append(uri)
+        print(songs)
+    except IndexError:
+        print(f"{song} doesn't exist in Spotify. Skipped.")
+
+playlist = sp.user_playlist_create(user_id, f"{year}-{month}-{day} Billboard 100")
+playlist_id = playlist["id"]
+sp.user_playlist_add_tracks(user=user_id, playlist_id=playlist_id, tracks=songs)
---
+++ Day_47/Amazon Deals/main.py
@@ -0,0 +1,29 @@
+import requests
+from bs4 import BeautifulSoup
+import smtplib
+
+my_email = "itnajitna@gmail.com"
+passw = "imwfxsmrztktngjh"
+
+account_sid ="ACc97d07942975958296f11ef0697129bd"
+auth_token ="c7765cd1effe6b95acc565ad2a50eeb8"
+
+Product_URL="https://www.amazon.in/gp/product/B0BQC1L5XW/ref=ox_sc_act_title_1?smid=A2GTG1HPYW8M2P&psc=1"
+HEADER={
+    "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36 Edg/113.0.1774.35",
+    "Accept-Language":"en-GB,en;q=0.9,en-US;q=0.8"
+}
+response=requests.get(url=Product_URL,headers=HEADER).text
+soup=BeautifulSoup(response,"lxml")
+price=int(soup.find("span",class_="a-price-whole").text.strip("."))
+title=soup.find("span", id="productTitle", class_="a-size-large product-title-word-break").text.strip("")
+
+if price<700:
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(user=my_email,password=passw)
+        connection.sendmail(
+        from_addr=my_email,
+        to_addrs="Kashifalikhan093@gmail.com",
+        msg=f"Subject:Ur Amazon Deal !!! \nOnly rs:{price} to get {title}".encode('utf-8')
+    )
